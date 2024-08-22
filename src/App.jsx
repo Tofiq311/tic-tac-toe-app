dca26a5a8d0568b30b563f07f77238053dca570f
Tofiq311 <ashik.tofiq.1@gmail.com>
2024-08-22 19:15:18 +0900

    deploy react to github

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TicTacToe from './Components/TicTacToe/TicTacToe'
+
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
+
